43b348d2ff4e4bed9478ec2d8c3c74190605e8e5
Bhavyeshshah <bhavyeshshah259@gmail.com>
2022-04-21 21:40:23 +0530

    Changes Done

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
+model = pickle.load(open('random_forest_regression_model_CarX.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
@@ -15,18 +15,26 @@ def predict():
     Fuel_Type_Diesel=0
     if request.method == 'POST':
         Year = int(request.form['Year'])
-        Present_Price=float(request.form['Present_Price'])
+        # Present_Price=float(request.form['Present_Price'])
+        mileage = int(request.form['Milegae'])
+        engine = int(request.form['Engine'])
+        max_power = int(request.form['Max _Power'])
+        seats = int(request.form['Seat'])
         Kms_Driven=int(request.form['Kms_Driven'])
         Kms_Driven2=np.log(Kms_Driven)
         Owner=int(request.form['Owner'])
-        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
-        if(Fuel_Type_Petrol=='Petrol'):
-                Fuel_Type_Petrol=1
-                Fuel_Type_Diesel=0
+        Fuel_Type=request.form['Fuel_Type_Petrol']
+        if(Fuel_Type=='Petrol'):
+                Fuel_Type=0
+                # Fuel_Type_Diesel=0
+        elif(Fuel_Type=='Diesel'):
+                Fuel_Type=1
+                # Fuel_Type_Diesel=0:
+        elif(Fuel_Type=='CNG'):
+                Fuel_Type=2
         else:
-            Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=1
-        Year=2020-Year
+             Fuel_Type=3
+        Year=2022-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
             Seller_Type_Individual=1
@@ -34,10 +42,10 @@ def predict():
             Seller_Type_Individual=0	
         Transmission_Mannual=request.form['Transmission_Mannual']
         if(Transmission_Mannual=='Mannual'):
-            Transmission_Mannual=1
-        else:
             Transmission_Mannual=0
-        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        else:
+            Transmission_Mannual=1
+        prediction=model.predict([[Year, Kms_Driven2, Fuel_Type, Seller_Type_Individual, Transmission_Mannual, Owner, mileage, engine, max_power, seats]])
         output=round(prediction[0],2)
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
